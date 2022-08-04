f2a01a270d93832058ac39ee88b15047b00591b8
Liza Kush <ekushnirchuk@yandex.ru>
2022-08-04 21:28:13 +0300

    task №4 done

--- task_final/solution.js
+++ task_final/solution.js
@@ -30,24 +30,26 @@
 //           "count": 2
 //         }
 //      ]
-//    }
+//    }:  
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address, goods, sum){
+	let data = {goods: [], order: {}, client: ""};
+	let countOfGoods =goods.length;
 
-    let countOfGoods = goods.length;
+	for (let i = 0; i < countOfGoods; i += 1) {
+        let abc = {   
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(abc);
+	}
+    data.order = {
+        address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat,
+        sum: sum
+        };
+        data.client = name + " " + phone;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+        let jsonData = JSON.stringify({data: data});
     return jsonData;
 }
